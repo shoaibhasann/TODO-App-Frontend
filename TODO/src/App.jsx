82b1340a7c875ac50d2be636ec783b31e71d16dc
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-05 20:59:29 +0530

    fix some bugs in api call

--- TODO/src/App.jsx
+++ TODO/src/App.jsx
@@ -7,42 +7,49 @@ import Register from './pages/Register';
 import { Toaster, toast } from 'react-hot-toast';
 import { Context, server } from './main';
 import axios from 'axios';
+import Landingpage from './pages/Landingpage';
 
 
 function App() {
   const { setUser, setIsAuthenticated, isAuthenticated, loading, setLoading } = useContext(Context);
-  console.log('app.js',isAuthenticated)
 
+// In your initial authentication check
   useEffect(() => {
     setLoading(true);
-    axios
-      .get(`${server}/user/`, {
-        withCredentials: true,
-      })
-      .then((response) => {
-        setUser(response.data.user);
-        setIsAuthenticated(true);
-        setLoading(false);
-      })
-      .catch((error) => {
-        toast.error(error.response.data.message);
-        setUser({});
-        setIsAuthenticated(false);
-        setLoading(false);
-      });
-  }, [])
-  
+    if (isAuthenticated) {
+      axios
+        .get(`${server}/user/`, {
+          withCredentials: true,
+        })
+        .then((response) => {
+          setUser(response.data.user);
+          setIsAuthenticated(true);
+          setLoading(false);
+        })
+        .catch((error) => {
+          toast.error(error.response.data.message);
+          setUser({});
+          setIsAuthenticated(false);
+          setLoading(false);
+        });
+    } else {
+      setLoading(false);
+    }
+  }, [isAuthenticated]);
+
+
   return (
-     <Router>
-      <Header/>
+    <Router>
+      <Header />
       <Routes>
-        <Route path='/'element={<Home/>}/>
-        <Route path='/login'element={<Login/>}/>
-        <Route path='/register'element={<Register/>}/>
+        <Route path="/" element={isAuthenticated ? <Navigate to="/home" /> : <Landingpage />}/>
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
-      <Toaster/>
-     </Router>
-  )
+      <Toaster />
+    </Router>
+  );
 }
 
 export default App
