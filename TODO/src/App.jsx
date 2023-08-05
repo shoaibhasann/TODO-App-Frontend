b08f5fa9069cd7ed836467b42ed9885260a3e7c1
shoaibhasann <shoaibhasan0940@gmail.com>
2023-08-05 21:40:39 +0530

    fix some bugs

--- TODO/src/App.jsx
+++ TODO/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from 'react';
-import { BrowserRouter as Router , Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router , Routes, Route, Navigate } from "react-router-dom";
 import Header from './components/Header';
 import Home from './pages/Home';
 import Login from './pages/Login';
